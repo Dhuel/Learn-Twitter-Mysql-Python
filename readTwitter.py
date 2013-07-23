4adef765ca9631e40a90d9d59135889de49d22f1
Dhuel <dhuelfisher@gmail.com>
2013-07-23 09:16:31 -0400

    progres

--- readTwitter.py
+++ readTwitter.py
@@ -1,4 +1,4 @@
 
 #TODO -Complete task 1 here
 #More stuff here 
-# Pretty sure i understand how to use modules but now i'm on the instillation art of it using pip  
+# Pretty sure i understand how to use modules but now i'm on the instillation part of it using pip  
